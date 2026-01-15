d5e5f144e34db215f76c0907a6cae9e59ecdd289
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-01-15 12:56:19 +0500

    some changes

--- root.js
+++ root.js
@@ -1,5 +1,29 @@
-const fs = require('fs');
-fs.writeFile('example.txt', 'hello world', (err) => {
-  if(err) throw err;
-  else console.log("File written successfully");
+let express = require('express');
+let app = express()
+
+
+
+let data = [
+  {   id: 1, name: "johan"},
+  {   id: 1, name: "johan"},
+  {   id: 1, name: "johan"},
+  {   id: 1, name: "johan"},
+  {   id: 1, name: "johan"},
+  {   id: 1, name: "johan"},
+  {   id: 1, name: "johan"},
+  {   id: 1, name: "johan"}
+]
+
+app.get('/mehboob', (req, res) => {
+  res.json(data)
 })
+
+app.get('/' , (req,resp) => {
+  resp.send("mm")
+})
+app.listen(3000, () => {
+  console.log("server is runing");
+  
+})
+
+let express = require(express);
