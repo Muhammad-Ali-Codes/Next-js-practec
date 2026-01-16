6d8dbd32faf69e46c6fadef1e03d005b4cd7d217
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-01-16 20:42:55 +0500

    some changes

--- root.js
+++ root.js
@@ -1,47 +1,11 @@
-let express = require('express');
-let app = express();
-app.use(express.json());
+const express = require('express');
+const userdata = require('./userData');
 
+const app = express();
 
-let data = [
-  { id: 1, name: "johan" },
-  { id: 2, name: "johan" },
-  { id: 3, name: "johan" },
-  { id: 4, name: "johan" }
-];
-
-
-app.get('/about', (req, res) => {
-   res.write("this is about page");
-   res.end();
-});
-
-
-app.get('/meer', (req, res) => {
-   res.write("this is meer page");
-   res.end();
-});
-app.get('/data', (req, res) => {
-  res.json(data);
-});
-
-app.get('/', (req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("papan papan bool");
-});
-
-
-
+app.use('/user', userdata);
 
 const port = 30001;
 app.listen(port, () => {
-  console.log("http://localhost:" + port);
-
+  console.log(`http://localhost:${port}`);
 });
-
-
-
-
-
-
-
